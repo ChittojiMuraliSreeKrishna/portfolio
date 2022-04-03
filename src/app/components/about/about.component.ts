ce8c90b634d2ef882c828b3b9418d67ab7eb3965
Chittoji Murali Sree Krishna <cmkrishna97@gmail.com>
2022-04-03 13:51:59 +0530

    updated till about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  myBackground = '../../../assets/Images/mine2.jpg'
+  aboutImg = '../../../assets/Images/mine3.jpg'
+
+  html = '../../../assets/Images/icons/html.png'
+  css = '../../../assets/Images/icons/css.png'
+  javaScript = '../../../assets/Images/icons/javascript.png'
+  python = '../../../assets/Images/icons/python.png'
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/college/college.component.ts
+++ src/app/components/college/college.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, Output } from '@angular/core';
-import {Images} from './mock-college'
 
 @Component({
   selector: 'app-college',
   templateUrl: './college.component.html',
   styleUrls: ['./college.component.css']
 })
-export class CollegeComponent implements OnInit {
-  @Output() images: any = Images
 
-  summerInternShipCode = 'https://github.com/ChittojiMuraliSreeKrishna/collegeProjects/tree/main/summerinternship'
+export class CollegeComponent implements OnInit {
+
+  summerIntern1 = '../../../assets/Images/college/cs5.png'
   miniProject1 = '../../../assets/Images/college/cs9.jpg'
+  
+  summerInternShipCode = 'https://github.com/ChittojiMuraliSreeKrishna/collegeProjects/tree/main/summerinternship'
+  miniProjectCode = 'https://github.com/ChittojiMuraliSreeKrishna/collegeProjects/tree/main/miniProject'
 
   constructor() { }
 
