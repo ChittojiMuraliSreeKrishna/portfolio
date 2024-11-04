6f7482c863e468a33a86379fecc2161f2cf165b5
muralichittoji <murali.chittoji@otsi.co.in>
2024-11-04 12:01:36 +0530

    updated few details

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,21 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
+  myBackground = '../../../assets/Images/mine2.jpg';
+  aboutImg = '../../../assets/Images/mine3.jpg';
 
-  myBackground = '../../../assets/Images/mine2.jpg'
-  aboutImg = '../../../assets/Images/mine3.jpg'
+  html = '../../../assets/Images/icons/html.png';
+  css = '../../../assets/Images/icons/css.png';
+  javaScript = '../../../assets/Images/icons/javascript.png';
+  python = '../../../assets/Images/icons/python.png';
+  springBoot = '../../../assets/Images/icons/springBoot.png';
+  flask = '../../../assets/Images/icons/flask.png';
+  scss = '../../../assets/Images/icons/sass.png';
+  bootStrap = '../../../assets/Images/icons/bootStrap.png';
+  react = '../../../assets/Images/icons/react.png';
+  postgresql = '../../../assets/Images/icons/postgresql.png';
+  mariadb = '../../../assets/Images/icons/mariadb.png';
+  mysql = '../../../assets/Images/icons/mysql.png';
+  php = '../../../assets/Images/icons/php.png';
 
-  html = '../../../assets/Images/icons/html.png'
-  css = '../../../assets/Images/icons/css.png'
-  javaScript = '../../../assets/Images/icons/javascript.png'
-  python = '../../../assets/Images/icons/python.png'
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
